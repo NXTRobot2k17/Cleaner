bea09ff33d0da0fcb8f1946665190f279d0c2e38
Felix <Felix@Felix6>
2017-05-18 13:22:36 +0200

    Motor Interface

--- src/MainProc/MainProc.java
+++ src/MainProc/MainProc.java
@@ -1,5 +1,4 @@
 package MainProc;
-
 import interfaces.*;
 import components.*;
 
---
+++ src/interfaces/IMotor.java
@@ -0,0 +1,10 @@
+package interfaces;
+import lejos.*;
+public interface IMotor {
+	public void forwards();
+	public void backwards();
+	public void setSpeed();
+	public void getspeed();
+	public void stop();
+	public void setport();
+}
