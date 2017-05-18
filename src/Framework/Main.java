a70bfba84c946e1af8c1c4a2222b7e8cbe32c24b
Karsten Dehren <karsten.dehren@brsmotorsport.de>
2017-05-18 12:35:40 +0200

    package segregation

---
+++ src/Framework/Main.java
@@ -0,0 +1,17 @@
+package Framework;
+
+import MainProc.*;
+
+
+public class Main {
+	static MainProc proc=new MainProc();
+	public static boolean keepAlive=true;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		proc.init();
+		while(keepAlive)
+			proc.run();
+	}
+
+}
---
+++ src/MainProc/MainProc.java
@@ -0,0 +1,17 @@
+package MainProc;
+
+import interfaces.*;
+import components.*;
+
+public class MainProc {
+
+	public void init()
+	{
+		
+	}
+	
+	public void run()
+	{
+		
+	}
+}
