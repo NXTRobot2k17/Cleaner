65551fddaa4d34d07b18c58b151e69ee9537ef07
Felix212 <felix.groove@googlemail.com>
2017-05-22 10:09:47 +0200

    Motor changed to Engine due to importnames

---
+++ src/components/Engine.java
@@ -0,0 +1,48 @@
+package components;
+
+import interfaces.IMotor;
+import lejos.robotics.RegulatedMotor;
+import lejos.nxt.NXTRegulatedMotor;
+public class Engine implements IMotor {
+
+	private RegulatedMotor Engine;
+	
+	public Engine(NXTRegulatedMotor m)
+	{
+		Engine = m;  //Motor.A, Motor.B, Motor.C
+	}
+	@Override
+	public void forwards() {
+		Engine.backward();
+	}
+
+	@Override
+	public void backwards() {
+		Engine.forward();
+	}
+
+	@Override
+	public void setSpeed(int speed) {
+		Engine.setSpeed(speed);
+	}
+
+	@Override
+	public void getspeed() {
+		Engine.getSpeed();
+	}
+
+	@Override
+	public void stop() {
+		Engine.stop();
+	}
+
+	@Override
+	public void setport() {
+		
+	}
+	@Override
+	public boolean isAlive() {
+		return false;
+	}
+
+}
--- src/interfaces/IMotor.java
+++ src/interfaces/IMotor.java
@@ -1,11 +1,11 @@
 package interfaces;
-import lejos.*;
 
 public interface IMotor {
 	public void forwards();
 	public void backwards();
-	public void setSpeed();
+	public void setSpeed(int speed);
 	public void getspeed();
 	public void stop();
 	public void setport();
+	public boolean isAlive();
 }
