f2f85f5d6674dbe2a51053b87dc1589e4ffbf582
Felix <Felix@felix4.Speedport_W_724V_Typ_A_05011603_05_020>
2017-05-31 20:51:53 +0200

    removed DisplayPackage constructor

--- src/components/DisplayPackage.java
+++ src/components/DisplayPackage.java
@@ -5,10 +5,4 @@ public class DisplayPackage {
 	public String sonicInfo;
 	public String MotorLeft;
 	public String Motorright;
-	public DisplayPackage(String sonar, String sonic, String ml, String mr) {
-		sonarInfo = sonar;
-		sonicInfo = sonic;
-		MotorLeft = ml;
-		Motorright = mr;
-	}
 }
