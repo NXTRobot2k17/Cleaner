7ea530c298be4434d1b52c38d3836836450df983
Felix212 <felix.groove@googlemail.com>
2017-06-08 11:10:34 +0200

    Removed Hardware Strings

--- src/components/Display.java
+++ src/components/Display.java
@@ -8,10 +8,10 @@ public class Display implements IDisplay {
 
 	@Override
 	public void update(DisplayPackage p) {
-		LCD.drawString("Sonic " + p.sonicInfo, 0, 0);
-		LCD.drawString("Sonar" + p.sonarInfo, 0, 1);
-		LCD.drawString("MotorLeft " + p.MotorLeft, 0, 2);
-		LCD.drawString("MotorRight " + p.Motorright, 0, 3);		
+		LCD.drawString(p.sonicInfo, 0, 0);
+		LCD.drawString(p.sonarInfo, 0, 1);
+		LCD.drawString(p.MotorLeft, 0, 2);
+		LCD.drawString(p.Motorright, 0, 3);		
 	}
 
 }
